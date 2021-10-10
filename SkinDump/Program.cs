5c539941e1cc5e2319e3f00cfe84adcbb43f4011
Jimmy Hoke <jimmy.hoke@yahoo.com>
2021-10-10 18:11:56 -0500

    Add project files.

---
+++ SkinDump/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SkinDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\assets\skins";
+            if (args.Length != 0)
+            {
+                path = args[0] + @"\assets\skins";
+            }
+            Directory.CreateDirectory(@"\skins");
+            foreach (string dirPath in Directory.GetDirectories(path))
+            {
+                foreach (string skin in Directory.GetFiles(dirPath))
+                {
+                    try
+                    {
+                        File.Copy(skin, @"\skins\" + Path.GetFileName(skin) + ".png");
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+            }
+            System.Diagnostics.Process.Start(@"\skins");
+        }
+    }
+}
